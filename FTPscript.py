ed1d5db997ab8fc88b530d16bb8334b8987fad0a
jason <hyunjun306@gmail.com>
2023-11-28 14:46:14 -0500

    sound, file db updates

--- FTPscript.py
+++ FTPscript.py
@@ -4,6 +4,7 @@ import subprocess
 import time
 import logging
 from playsound import playsound
+import sqlite3
 
 
 beep1 = './assets/beep-01a.wav'
@@ -12,6 +13,7 @@ come_get_it = './assets/come_get_it.wav'
 no_files = './assets/car_horn_x.wav'
 good_to_go = './assets/toot2_x.wav'
 # Function to play a sound
+python_script_path = './status.py'
 
 
 def play_notification_sound(sound_path):
@@ -38,8 +40,8 @@ ftp_configs = [
     },
     {
         "network_name": 'Yoo',
-        "ftp_server": '10.226.121.80',
-        "username": 'ftp-user2',
+        "ftp_server": '192.168.1.54',
+        "username": 'ftp-villageA',
         "password": '1234',
         "local_directory": r'C:\Users\hyunj\OneDrive\Desktop\MVP'
     },
@@ -47,10 +49,39 @@ ftp_configs = [
 ]
 
 
-def delete_files(ftp, file_list):
-    for remote_filename in file_list:
-        ftp.delete(remote_filename)
-        print(f'Deleted {remote_filename} from the {ftp.host}')
+def get_next_delivery():
+    conn = sqlite3.connect('file_delivery.db')
+    cursor = conn.cursor()
+
+    # Example query to get the next file to be delivered
+    cursor.execute(
+        "SELECT filename, SourceVillage, DestinationVillage FROM FileStatuses WHERE Status = 'In Transit (Yellow)' LIMIT 1;")
+    result = cursor.fetchone()
+
+    conn.close()
+    # Returns a tuple (filename, SourceVillage, DestinationVillage)
+    return result
+
+
+def mark_as_delivered(filename):
+    conn = sqlite3.connect('file_delivery.db')
+    cursor = conn.cursor()
+
+    # Example query to mark the file as delivered
+    cursor.execute(
+        "UPDATE FileStatuses SET Status = 'Delivered (Green)' WHERE filename = ?;", (filename,))
+
+    conn.commit()
+    conn.close()
+
+
+def delete_files(ftp, downloaded_files, upload_files):
+    print("######################## DELETING FILES ########################")
+    for remote_filename in downloaded_files:
+        print(remote_filename)
+        if remote_filename not in upload_files:
+            ftp.delete(remote_filename)
+            print(f'Deleted {remote_filename} from the {ftp.host}')
 
 
 def delete_local_files(local_directory):
@@ -77,7 +108,7 @@ def is_connected_to_wifi_network(network_name):
         return False
 
 
-def download_files(ftp, username, local_directory, downloaded_files, log_file):
+def download_files(ftp, username, local_directory, downloaded_files, upload_files):
     file_list = ftp.nlst()
 
     # Create the local directory if it doesn't exist
@@ -89,7 +120,7 @@ def download_files(ftp, username, local_directory, downloaded_files, log_file):
         play_notification_sound(come_get_it)
 
     for remote_filename in file_list:
-        if remote_filename not in downloaded_files:
+        if remote_filename not in downloaded_files and remote_filename not in upload_files:
             # Check the file extension and skip downloading log files
             if not remote_filename.endswith('.log'):
                 local_filepath = os.path.join(local_directory, remote_filename)
@@ -102,17 +133,16 @@ def download_files(ftp, username, local_directory, downloaded_files, log_file):
 
                 print(f'Downloaded {remote_filename} to {local_filepath}')
                 downloaded_files.append(remote_filename)  # Append to the list
-
     logging.shutdown()
 
 
-def upload_files(ftp, local_directory, uploading_files):
+def upload_files_to_ftp(ftp, local_directory, upload_files, ftp_config):
     local_files = os.listdir(local_directory)
 
     # Check if there are files to upload before proceeding
     if local_files:
         for local_filename in local_files:
-            if local_filename not in uploading_files:
+            if local_filename not in upload_files:
                 if not local_filename.endswith(".log"):
                     local_filepath = os.path.join(
                         local_directory, local_filename)
@@ -120,15 +150,24 @@ def upload_files(ftp, local_directory, uploading_files):
                         ftp.storbinary(f'STOR {local_filename}', local_file)
 
                     # Log the action on the client
-                    log_message = f'From Truck {local_directory} upload {local_filename} to {ftp.host}'
+                    log_message = f'From Truck {local_directory} upload {local_filename} to {ftp_config["username"]} {ftp.host}'
                     logging.info(log_message)
 
-                    print(f'Uploaded {local_filename} to the FTP server')
-                    uploading_files.append(local_filename)
+                    print(
+                        f'Uploaded {local_filename} to the {ftp_config["username"]} {ftp_config["ftp_server"]}')
+                    upload_files.append(local_filename)
         logging.shutdown()
 
     else:
         print("No files to upload.")
+
+
+def remove_files_local_directory(local_files):
+    for local_filename in local_files:
+        if not local_filename.endswith(".log"):
+            local_filepath = os.path.join(
+                r'C:\Users\hyunj\OneDrive\Desktop\MVP', local_filename)
+            os.remove(local_filepath)
 
 
 def upload_log_files(ftp, local_directory):
@@ -147,12 +186,15 @@ def upload_log_files(ftp, local_directory):
 
                 print(f'Uploaded {local_filename} to the FTP server')
         logging.shutdown()
-        play_notification_sound(good_to_go)
+        # play_notification_sound(good_to_go)
     else:
         print("No files to upload")
 
 
-def connect_to_ftp_server(ftp_config):
+def connect_to_ftp_server(ftp, ftp_config, downloaded_files, upload_files):
+    # Initialize the list for local files
+    local_files = [filename for filename in os.listdir(
+        r'C:\Users\hyunj\OneDrive\Desktop\MVP') if not filename.endswith('.log')]
 
     if is_connected_to_wifi_network(ftp_config["network_name"]):
         try:
@@ -162,50 +204,86 @@ def connect_to_ftp_server(ftp_config):
             retry_count = 0
             while retry_count < max_retries:
                 try:
-                    ftp = FTP()
                     ftp.connect(ftp_config["ftp_server"])
                     ftp.login(ftp_config["username"], ftp_config["password"])
                     print(
-                        f'Truck arrived at {ftp_config["network_name"]} (FTP server)')
+                        f'Truck arrived at {ftp_config["username"]} {ftp_config["ftp_server"]}')
 
-                    # Check for files to download
-                    downloaded_files = []
-                    print(f'Truck checking for files to pick up...')
+                    if local_files:
+                        print(f"There are loads in Truck")
+                        # Upload files to the FTP server
+                        upload_files_to_ftp(
+                            ftp, ftp_config["local_directory"], upload_files, ftp_config)
+                        upload_log_files(ftp, ftp_config["local_directory"])
+                        remove_files_local_directory(local_files)
+                    print("Upload_files", upload_files)
+                    print(
+                        "################################ Truck Files deleted ############################################")
+                    # Download files from the FTP server
+                    print(
+                        f'Truck checking for files to pick up at {ftp_config["username"]}')
                     download_files(
-                        ftp, ftp_config["username"], ftp_config["local_directory"], downloaded_files, client_log_file)
+                        ftp, ftp_config["username"], ftp_config["local_directory"], downloaded_files, upload_files)
 
                     if downloaded_files:
                         print(f'Truck found files to pick up!')
                         upload_log_files(ftp, ftp_config["local_directory"])
 
-                    delete_files(ftp, downloaded_files)
+                    delete_files(ftp, downloaded_files, upload_files)
+                    upload_files = []
+                    print(
+                        "#################################################################################################")
+
                     ftp.quit()
                     break  # Break the loop if the connection and file check are successful
                 except TimeoutError:
                     retry_count += 1
                     print(
-                        f'Truck waiting at {ftp_config["network_name"]}... (Attempt {retry_count}/{max_retries})')
+                        f'Truck waiting at {ftp_config["username"]} { ftp_config["ftp_server"]}... (Attempt {retry_count}/{max_retries})')
                     time.sleep(retry_interval)
             else:
                 print(
-                    f'Truck unable to connect to {ftp_config["network_name"]} after {max_retries} attempts. Moving on...')
+                    f'Truck unable to connect to {ftp_config["username"]} {ftp_config["ftp_server"]} after {max_retries} attempts. Moving on...')
                 play_notification_sound(no_files)
                 play_notification_sound(no_files)
 
         except Exception as e:
             print(
-                f"Error with FTP server {ftp_config['network_name']}: {str(e)}")
+                f"Error with FTP server {ftp_config['username']}: {str(e)}")
     else:
         print(
-            f'Truck not connected to {ftp_config["network_name"]}. Moving on...')
+            f'Truck not connected to {ftp_config["username"]}. Moving on...')
+    # Run the status script to update FileStatuses
+    subprocess.run(['python', python_script_path])
 
 
+# Initialize the list to keep track of downloaded files
 # Truck driver's routine
 while True:
-    files_downloaded = False  # Initialize a variable to track whether files are downloaded
-
     for ftp_config in ftp_configs:
-        connect_to_ftp_server(ftp_config)
+        downloaded_files = []
+
+        upload_files = []
+
+        ftp = FTP()  # Create a new FTP instance for each village
+        connect_to_ftp_server(ftp, ftp_config, downloaded_files, upload_files)
+
+        # delivery_info = get_next_delivery()
+        # if delivery_info:
+        #     filename, source_village, destination_village = delivery_info
+        #     print(
+        #         f'Truck delivering {filename} from {source_village} to {destination_village}')
+
+        #     try:
+        #         # Perform FTP operations here (download, upload, etc.)
+        #         # connect_to_ftp_server(ftp, ftp_config)
+
+        #         # Mark the file as delivered in the database
+        #         mark_as_delivered(filename)
+        #     except Exception as e:
+        #         print(f"Error with {str(e)}")
+        # else:
+        #     print("No more deliveries at the moment.")
 
     # Truck driver takes a break before checking the next village
     time.sleep(5)
---
+++ status.py
@@ -0,0 +1,240 @@
+import sqlite3
+import os
+import re
+import time
+
+DATABASE_PATH = 'file_delivery.db'
+
+
+def create_tables():
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    cursor.execute('DROP TABLE IF EXISTS FileStatuses')
+    cursor.execute('DROP TABLE IF EXISTS UserAccounts')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS UserAccounts (
+            Username TEXT PRIMARY KEY,
+            Village TEXT,
+            Email TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS FileStatuses (
+            Filename TEXT PRIMARY KEY,
+            SourceVillage TEXT,
+            Sender TEXT,
+            DestinationVillage TEXT,
+            DestinationUser TEXT,
+            Status TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
+def add_user_account(username, village, email):
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT OR REPLACE INTO UserAccounts (Username, Village, Email)
+        VALUES (?, ?, ?)
+    ''', (username, village, email))
+
+    conn.commit()
+    conn.close()
+
+
+def get_users_in_village(village):
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        'SELECT Username, Email FROM UserAccounts WHERE Village = ?', (village,))
+    users = cursor.fetchall()
+
+    conn.close()
+    return users
+
+
+class Truck:
+    def __init__(self):
+        self.village_users = {}
+
+    def receive_user_information(self, village, users):
+        self.village_users[village] = users
+
+    def update_user_information(self, village, users):
+        conn = sqlite3.connect(DATABASE_PATH)
+        cursor = conn.cursor()
+
+        # Update the user information in the village's database
+        for username, email in users:
+            cursor.execute('''
+                INSERT OR REPLACE INTO UserAccounts (Username, Village, Email)
+                VALUES (?, ?, ?)
+            ''', (username, village, email))
+
+        conn.commit()
+        conn.close()
+
+    def distribute_user_information(self):
+        for destination_village, users in self.village_users.items():
+            for source_village, source_users in self.village_users.items():
+                if destination_village != source_village:
+                    # Distribute user information from source village to destination village
+                    self.update_user_information(
+                        destination_village, source_users)
+
+
+def clear_table(table_name):
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute(f'DELETE FROM {table_name}')
+
+    conn.commit()
+    conn.close()
+
+
+# Function to get file statuses from the log file and update the database
+
+
+def update_file_statuses(log_file_path, directory_path):
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+
+    file_statuses = {}
+
+    def update_file_status(filename, source_village, sender, destination_user, status):
+        conn = sqlite3.connect(DATABASE_PATH)
+        cursor = conn.cursor()
+        # # Print the value of destination_user for debugging
+        # print(f"Destination User: {destination_user}")
+
+        # Query the UserAccounts table to get the destination village
+        cursor.execute('''
+            SELECT * FROM UserAccounts WHERE LOWER(Username) = LOWER(?)
+        ''', (destination_user,))
+        result = cursor.fetchone()
+        # print(result)
+        destination_village = result[1]
+
+        # Check if the destination user exists in the UserAccounts table
+        if destination_village:
+            # Convert filenames and username to lowercase for case-insensitive comparison
+            filename_lower = filename.lower()
+            sender_lower = sender.lower()
+            source_info = f"from {source_village}"
+            status_message = f"{filename}: {source_info} {sender_lower} -> {destination_village} - {status} (Sender: {sender}, Receiver: {destination_user})"
+            print(status_message)  # Print the status message
+
+            cursor.execute('''
+                SELECT * FROM FileStatuses WHERE Filename = ?
+            ''', (filename_lower,))
+            existing_entry = cursor.fetchone()
+
+            if existing_entry:
+                # Entry exists, update only the Status field
+                cursor.execute('''
+                    UPDATE FileStatuses
+                    SET Status = ?
+                    WHERE Filename = ?
+                ''', (status, filename_lower))
+            else:
+                # Entry doesn't exist, insert a new row
+                cursor.execute('''
+                    INSERT INTO FileStatuses (Filename, SourceVillage, Sender, DestinationVillage, DestinationUser, Status)
+                    VALUES (?, ?, ?, ?, ?, ?)
+                ''', (filename_lower, source_village, sender, destination_village, destination_user, status))
+
+        else:
+            # Handle the case where the destination user is not found
+            print(
+                f"Destination user '{destination_user}' not found in UserAccounts table.")
+        conn.commit()
+        conn.close()
+
+    def get_status(line):
+        if "download" in line:
+            return "In Transit (Yellow)"
+        elif "upload" in line:
+            return "Delivered (Green)"
+        else:
+            return "Other (Gray)"
+
+    with open(log_file_path, 'r') as log_file:
+        download_pattern = re.compile(
+            r"From (.+?) download (.+?) to Truck (.+)")
+        upload_pattern = re.compile(r"From (.+?) upload (.+?) to (.+)")
+
+        source_user = None  # Variable to store the source user during processing
+
+        for line in log_file:
+            if ".log" in line:
+                source_user_match = re.search(r"From (.+?) ", line)
+                if source_user_match:
+                    source_user = source_user_match.group(1)
+                continue
+
+            status = get_status(line)
+            download_match = download_pattern.search(line)
+            upload_match = upload_pattern.search(line)
+            # update_file_status(filename, source_village, sender, destination_user, status):
+            if download_match:
+                uploader, filename, destination = download_match.groups()
+
+                # Split the filename by underscore and take the second part (receiver)
+                sender = filename.split('_')[0]
+                receiver = filename.split('_')[1]
+                destination_village, destination_user = destination.split('_')
+                destination_user = destination_user.split('.')[0]
+                update_file_status(
+                    filename, uploader, sender, destination_user, status)
+            if upload_match:
+                uploader, filename, destination = upload_match.groups()
+                # print(uploader, filename, destination)
+                # Split the filename by underscore and take the second part (receiver)
+                sender = filename.split('_')[0]
+                receiver = filename.split('_')[1]
+                receiver = receiver.split('.')[0]
+
+                update_file_status(
+                    filename, uploader, sender, receiver, status)
+
+    # List files in the directory
+    files_in_directory = [f for f in os.listdir(
+        directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+
+    conn.commit()
+    conn.close()
+
+
+# Example usage
+
+create_tables()
+
+# Example usage
+log_file_path = r'C:\Users\hyunj\OneDrive\Desktop\MVP\client.log'
+directory_path = r'C:\Users\hyunj\OneDrive\Desktop\MVP'
+
+# Village A adds new users
+add_user_account("Alice", "ftp-villageA", "alice@email.com")
+add_user_account("Bob", "ftp-villageA", "bob@email.com")
+
+# Village B adds new users
+add_user_account("Jason", "ftp-user", "jason@email.com")
+add_user_account("Ellen", "ftp-user", "ellen@email.com")
+add_user_account("Ace", "ftp-user", "ace@email.com")
+
+
+update_file_statuses(log_file_path, directory_path)
+
+# Print File Statuses
+conn = sqlite3.connect(DATABASE_PATH)
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM FileStatuses')
+file_statuses = cursor.fetchall()
+conn.close()
